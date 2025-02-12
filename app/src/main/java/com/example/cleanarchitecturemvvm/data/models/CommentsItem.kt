89755a7584e1905e90cd70818fa44cf500910dad
Sandeep Patil <snsan1109@gmail.com>
2025-02-12 16:53:50 +0530

    data class updated

--- app/src/main/java/com/example/cleanarchitecturemvvm/data/models/CommentsItem.kt
+++ app/src/main/java/com/example/cleanarchitecturemvvm/data/models/CommentsItem.kt
@@ -1,9 +1,11 @@
 package com.example.cleanarchitecturemvvm.data.models
 
+import androidx.versionedparcelable.ParcelField
+
 data class CommentsItem(
     val body: String,
     val email: String,
-    val id: Int,
+    val id: Int=0,
     val name: String,
     val postId: Int
 )
